eab7b7406d652c6280d3cba9449d6ae1720cb562
Yurockkk <r00631026@ntu.edu.tw>
2018-02-09 20:47:30 -0500

    Clean the code using Lint

--- app/src/main/java/com/example/yubo/bluepi/WifiDialogFragment.java
+++ app/src/main/java/com/example/yubo/bluepi/WifiDialogFragment.java
@@ -7,7 +7,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatDialogFragment;
 import android.text.Editable;
 import android.text.TextWatcher;
